44459c371da6fbd2b5e9af53d2b8d49ce2a0f3e0
aminyl <ellallopll@gmail.com>
2017-10-01 01:36:21 +0900

    no message

---
+++ Assets/GenerateGraph.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGraph : MonoBehaviour
+{
+	public GameObject point;
+	public DisplayData displayData;
+	int rawValue;
+
+	// Use this for initialization
+	void Start ()
+	{
+		displayData.Initialize ();
+		// displayData.controller.UpdateRawdataEvent += OnUpdateUpdateRawdata;
+		StartCoroutine(IGeneratePoint());
+	}
+	
+	void UpdateRawValue (int value)
+	{
+		rawValue = value;
+	}
+
+	float GetHeight ()
+	{
+		return (float)rawValue / 200;
+	}
+
+	Vector3 GetPosition ()
+	{
+		return new Vector3 (0, GetHeight (), 0);
+	}
+
+	Vector3 GetVelocity ()
+	{
+		return new Vector3 (2f, 0f, 0f);
+	}
+
+	void OnUpdateUpdateRawdata (int value)
+	{
+		UpdateRawValue (value);
+		GeneratePoint ();
+	}
+
+	void GeneratePoint ()
+	{
+		GameObject p = Instantiate (point);
+		p.GetComponent<PointController> ().Initialize (gameObject, GetPosition (), GetVelocity ());
+	}
+
+	IEnumerator IGeneratePoint ()
+	{
+		while (true) {
+			UpdateRawValue (displayData.raw);
+			GeneratePoint ();
+			yield return null;
+		}
+	}
+}
---
+++ Assets/PointController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointController : MonoBehaviour
+{
+	Vector3 velocity;
+
+	public void Initialize (GameObject parent, Vector3 position, Vector3 velocity)
+	{
+		transform.parent = parent.transform;
+		transform.position = position;
+		this.velocity = velocity;
+		StartCoroutine (Move ());
+	}
+
+	bool checkPosition ()
+	{
+		return transform.position.x > 9;
+	}
+
+	IEnumerator Move ()
+	{
+		while (true) {
+			transform.position += velocity * Time.deltaTime;
+			if (checkPosition ())
+				break;
+			yield return null;
+		}
+		Destroy (gameObject);
+	}
+}
